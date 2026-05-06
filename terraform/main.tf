6eaf9f1d430591fab4e7a0592d97637e7f27d071
brunela-1 <valeriatbw@gmail.com>
2026-05-06 05:23:50 -0500

    feat: habilitar versionado y cifrado en s3

--- terraform/main.tf
+++ terraform/main.tf
@@ -58,3 +58,21 @@ resource "random_id" "suffix" {
 resource "aws_s3_bucket" "images" {
   bucket = "image-processor-${var.environment}-${random_id.suffix.hex}"
 }
+
+resource "aws_s3_bucket_versioning" "images" {
+  bucket = aws_s3_bucket.images.id
+
+  versioning_configuration {
+    status = "Enabled"
+  }
+}
+
+resource "aws_s3_bucket_server_side_encryption_configuration" "images" {
+  bucket = aws_s3_bucket.images.id
+
+  rule {
+    apply_server_side_encryption_by_default {
+      sse_algorithm = "AES256"
+    }
+  }
+}

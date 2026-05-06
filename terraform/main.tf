dd0b0881e3cda01b0823eaa7b92db7979d23ee08
brunela-1 <valeriatbw@gmail.com>
2026-05-06 06:22:28 -0500

    feat: conectar s3 con sqs mediante notificaciones

--- terraform/main.tf
+++ terraform/main.tf
@@ -134,3 +134,38 @@ resource "aws_lambda_function" "crop" {
   filename         = "../lambdas/crop/crop.zip"
   source_code_hash = filebase64sha256("../lambdas/crop/crop.zip")
 }
+
+resource "aws_apigatewayv2_api" "main" {
+  name          = "image-api-${var.environment}"
+  protocol_type = "HTTP"
+}
+
+resource "aws_apigatewayv2_integration" "upload" {
+  api_id                 = aws_apigatewayv2_api.main.id
+  integration_type       = "AWS_PROXY"
+  integration_uri        = aws_lambda_function.upload.invoke_arn
+  payload_format_version = "2.0"
+}
+
+resource "aws_apigatewayv2_route" "upload" {
+  api_id    = aws_apigatewayv2_api.main.id
+  route_key = "POST /upload"
+
+  target = "integrations/${aws_apigatewayv2_integration.upload.id}"
+}
+
+resource "aws_apigatewayv2_stage" "default" {
+  api_id      = aws_apigatewayv2_api.main.id
+  name        = "$default"
+  auto_deploy = true
+}
+
+resource "aws_s3_bucket_notification" "notify" {
+  bucket = aws_s3_bucket.images.id
+
+  queue {
+    queue_arn    = aws_sqs_queue.main.arn
+    events       = ["s3:ObjectCreated:*"]
+    filter_prefix = "uploads/"
+  }
+}
